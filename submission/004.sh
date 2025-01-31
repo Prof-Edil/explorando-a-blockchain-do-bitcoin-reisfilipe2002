0ad40886e5cf1fa2cb2d57936795d81bb9cefa2a
Filipe Barbosa Reis <62529116+reisfilipe2002@users.noreply.github.com>
2025-01-31 14:41:19 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # RPC connection details
-RPC_CONNECT="84.247.182.145"
-RPC_USER="user_222"
-RPC_PASSWORD="oHUfdkZMEmJM"
+
 
 # Extended Public Key (XPUB)
 XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
@@ -15,10 +13,10 @@ DERIVATION_PATH="/100"
 DESCRIPTOR="tr($XPUB/$DERIVATION_PATH)"
 
 # Get descriptor checksum
-DESCRIPTOR_CHECKSUM=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getdescriptorinfo "$DESCRIPTOR" | jq -r '.descriptor')
+DESCRIPTOR_CHECKSUM=$(bitcoin-cli  getdescriptorinfo "$DESCRIPTOR" | jq -r '.descriptor')
 
 # Get derived Taproot address
-ADDRESS=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD deriveaddresses "$DESCRIPTOR_CHECKSUM" | jq -r '.[0]')
+ADDRESS=$(bitcoin-cli  deriveaddresses "$DESCRIPTOR_CHECKSUM" | jq -r '.[0]')
 
 # Output the Taproot address
 echo "$ADDRESS"
