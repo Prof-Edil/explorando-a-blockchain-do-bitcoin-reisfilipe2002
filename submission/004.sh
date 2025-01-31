e1e8ddf3270be92c84e43599752df4151c517a23
Filipe Barbosa Reis <62529116+reisfilipe2002@users.noreply.github.com>
2025-01-31 11:15:41 -0300

    =Solving question 4

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,24 @@
-# Using descriptors, compute the taproot address at index 100 derived from this extended public key:
-#   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+#!/bin/bash
+
+# RPC connection details
+RPC_CONNECT="84.247.182.145"
+RPC_USER="user_222"
+RPC_PASSWORD="oHUfdkZMEmJM"
+
+# Extended Public Key (XPUB)
+XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+
+# Derivation path for index 100 (BIP-86 Taproot standard)
+DERIVATION_PATH="0/100"
+
+# Descriptor for Taproot (BIP-86)
+DESCRIPTOR="tr($XPUB/$DERIVATION_PATH)"
+
+# Get descriptor checksum
+DESCRIPTOR_CHECKSUM=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getdescriptorinfo "$DESCRIPTOR" | jq -r '.descriptor')
+
+# Get derived Taproot address
+ADDRESS=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD deriveaddresses "$DESCRIPTOR_CHECKSUM" | jq -r '.[0]')
+
+# Output the Taproot address
+echo "$ADDRESS"
